45ba730c95f8759f97511aae02a1126573d1e708
siyuan.wang2015@gmail.com <siyuan.wang2015@gmail.com>
2026-04-26 22:50:52 -0400

    update treesitter for new branch and add lean plugins

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -16,9 +16,9 @@ return {
                     "clangd",
                     "eslint",
                     "ts_ls",
-                }
-
+                },
             })
+
             vim.lsp.enable("pyright")
             vim.lsp.enable("lua_ls")
             vim.lsp.enable("jdtls")
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -2,43 +2,37 @@ return {
     {
         "nvim-treesitter/nvim-treesitter",
         version = false,
+        branch = "main",
         build = ":TSUpdate",
         event = { "BufReadPost", "BufNewFile" },
         cmd = { "TSUpdate", "TSInstall", "TSLog", "TSUninstall" },
         config = function()
-            require('nvim-treesitter.configs').setup({
-                ensure_installed = {
-                    "bash",
-                    "cmake",
-                    "cpp",
-                    "c",
-                    "diff",
-                    "gitignore",
-                    "git_rebase",
-                    "gitcommit",
-                    "lua",
-                    "make",
-                    "html",
-                    "javascript",
-                    "typescript",
-                    "json",
-                    "python",
-                    "lua",
-                    "luadoc",
-                    "rust",
-                    "markdown",
-                    "markdown_inline",
-                    "latex",
-                    "vim",
-                    "vimdoc",
-                },
-                highlight = {
-                    enable = true,
-                },
-                indent = {
-                    enable = true,
-                }
-            })
+            require('nvim-treesitter.install').prefer_git = true,
+            require('nvim-treesitter').install {
+                "bash",
+                "cmake",
+                "cpp",
+                "c",
+                "diff",
+                "gitignore",
+                "git_rebase",
+                "gitcommit",
+                "lua",
+                "make",
+                "html",
+                "javascript",
+                "typescript",
+                "json",
+                "python",
+                "lua",
+                "luadoc",
+                "rust",
+                "markdown",
+                "markdown_inline",
+                "latex",
+                "vim",
+                "vimdoc",
+            }
         end
     }
 }
